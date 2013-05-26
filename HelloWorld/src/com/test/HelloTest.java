6dab54177bd11cf66e8ee5464917665f1e719e0e
alexc <alex.chan.ly@gmail.com>
2013-05-26 10:32:32 +0800

    2nd commit
    Add new HelloUser.java
    
    Change-Id: I0bf383c560181e9de0de0d8e18259bb282cc0e86
    Signed-off-by: alexc <alex.chan.ly@gmail.com>

---
+++ HelloWorld/src/com/test/HelloTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class HelloTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+		System.out.println("2nd Hello World !");
+
+	}
+
+}
