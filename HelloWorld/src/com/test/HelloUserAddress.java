84d2f49d6063451cba4f8226073ea1b14ebf14aa
alexc <alex.chan.ly@gmail.com>
2013-06-12 09:28:12 +0800

    4th Commit
    Add new java HelloUserContact.java, amend HelloTest.java
    
    Signed-off-by: alexc <alex.chan.ly@gmail.com>

---
+++ HelloWorld/src/com/test/HelloUserAddress.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class HelloUserAddress {
+	private String address;
+	private String district;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	
+	
+}
---
+++ HelloWorld/src/com/test/HelloUserContact.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class HelloUserContact {
+	private String whatAppsID;
+	private String twisterID;
+	
+	public String getWhatAppsID() {
+		return whatAppsID;
+	}
+	public void setWhatAppsID(String whatAppsID) {
+		this.whatAppsID = whatAppsID;
+	}
+	public String getTwisterID() {
+		return twisterID;
+	}
+	public void setTwisterID(String twisterID) {
+		this.twisterID = twisterID;
+	}
+	
+	
+}
